5572ec94bc44f2ca943a6b5e5f1d0bd2a14f3789
naeemy <catwinnerg@gmail.com>
2024-01-03 12:56:33 +0700

    fix tape equilibrium

--- python/lesson3/tape_equilibrium.py
+++ python/lesson3/tape_equilibrium.py
@@ -8,13 +8,10 @@ def solution(A):
     sum_of_all = sum(A)
     first_half = 0
     P = inf
-    for e in A:
+    for e in A[:-1]:
         first_half += e
         S = sum_of_all - first_half
         D = abs(first_half - S)
         P = D if D < P else P
 
     return P
-
-
-[-10, -20, -30, -40, 100]
